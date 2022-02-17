25b79f387cfea7a8d8a8a4ed3fa2e1f1c20a7cc8
subham-byte <20BCS9246@cuchd.in>
2022-02-17 14:00:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function showTime(){
+    var date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    var session = "AM";
+
+    
+    if(h>12){
+        session = "PM";
+        h-=12;
+    }
+    if(h==0){
+        h = 12;
+    }
+    
+    h = h<10 ? "0"+h : h;
+    m = m<10 ? "0"+m : m;
+    s = s<10 ? "0"+s : s;
+    var time = h + ":" + m + ":" + s + session;
+    document.querySelector("#clock").innerHTML = time;
+}
+setInterval(() => {
+    showTime();
+}, 1000);
